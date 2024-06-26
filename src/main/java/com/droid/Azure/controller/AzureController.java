3b2283fee0819ada31b53ab70de37fce024e4e1b
imdr0id <64463589+imdr0id@users.noreply.github.com>
2024-06-26 23:00:31 +0800

    first

---
+++ src/main/java/com/droid/Azure/controller/AzureController.java
@@ -0,0 +1,17 @@
+package com.droid.Azure.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
+
+
+
+}
